cb1ff35fd572e50f452cd865d5d572ba5f252c44
Ahmed <01099517574med@gmail.com>
2025-01-31 00:18:27 +0200

    create page view item

---
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hup/core/utils/app_images.dart';
+import 'package:fruits_hup/features/on_boarding/presentation/views/widgets/page_view_item.dart';
+
+class OnBoardingPageView extends StatelessWidget {
+  const OnBoardingPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        PageViewItem(
+          image: Assets.imagesPageViewItem1Image,
+          backgroundimage: Assets.imagesPageViewItem1BackgroundImage,
+          subtitle:
+              'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('مرحبًا بك ف'),
+              Text('Fruit'),
+              Text('HUB'),
+            ],
+          ),
+        ),
+        PageViewItem(
+          image: Assets.imagesPageViewItem2Image,
+          backgroundimage: Assets.imagesPageViewItem2BackgroundImage,
+          subtitle:
+              'نقدم لك أفضل الفواكه المختارة بعناية. اطلع على التفاصيل والصور والتقييمات لتتأكد من اختيار الفاكهة المثالية',
+          title: Text(
+            'ابحث و تسوق',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xff0C0D0D),
+              fontSize: 23,
+              fontFamily: 'Cairo',
+              fontWeight: FontWeight.w700,
+              height: 0,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/features/on_boarding/presentation/views/widgets/page_view_item.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PageViewItem extends StatelessWidget {
+  const PageViewItem(
+      {super.key,
+      required this.image,
+      required this.backgroundimage,
+      required this.subtitle,
+      required this.title});
+
+  final String image, backgroundimage;
+  final String subtitle;
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: SvgPicture.asset(
+                  backgroundimage,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: SvgPicture.asset(
+                  image,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16),
+                child: Text('تخط'),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 64,
+        ),
+        title,
+        SizedBox(
+          height: 24,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: Text(
+            subtitle,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    );
+  }
+}
