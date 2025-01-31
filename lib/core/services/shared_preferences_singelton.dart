02ff442d6bf53745de9e5c005e294552597d035c
Ahmed <01099517574med@gmail.com>
2025-01-31 23:37:46 +0200

    create shared preferances singelton

---
+++ lib/core/services/shared_preferences_singelton.dart
@@ -0,0 +1,16 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesSingleton {
+  static late SharedPreferences _insatance;
+  static Future<void> init() async {
+    _insatance = await SharedPreferences.getInstance();
+  }
+
+  static setBool(String key, bool value) {
+    _insatance.setBool(key, value);
+  }
+
+  static getBool(String key) {
+    return _insatance.getBool(key) ?? false;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hup/core/helper_function/on_generate_routes.dart';
+import 'package:fruits_hup/core/services/shared_preferences_singelton.dart';
 import 'package:fruits_hup/features/splash/presentation/views/splash_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'generated/l10n.dart';
 
-void main() {
+void main() async {
+  await SharedPreferencesSingleton.init();
   runApp(const FruitHup());
 }
 
