13a0aff59171cbe435a1b92ae749b655f0ac2a0e
Ahmed <01099517574med@gmail.com>
2025-02-02 00:29:01 +0200

    create custom check box

--- lib/features/auth/presentation/views/widget/terms_and_condition.dart
+++ lib/features/auth/presentation/views/widget/terms_and_condition.dart
@@ -1,67 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hup/constants.dart';
 import 'package:fruits_hup/core/utils/app_text_styles.dart';
+import 'package:fruits_hup/features/auth/presentation/views/widget/custom_check_box.dart';
 
 import '../../../../../core/utils/app_colors.dart';
 
-class TermsAndConditionWidget extends StatelessWidget {
+class TermsAndConditionWidget extends StatefulWidget {
   const TermsAndConditionWidget({super.key});
 
   @override
+  State<TermsAndConditionWidget> createState() =>
+      _TermsAndConditionWidgetState();
+}
+
+class _TermsAndConditionWidgetState extends State<TermsAndConditionWidget> {
+  bool isTermesAccepted = false;
+  @override
   Widget build(BuildContext context) {
-    return Transform.translate(
-      offset: const Offset(14, 0),
-      child: Row(
-        children: [
-          Checkbox(
-            value: false,
-            onChanged: (value) {},
-          ),
-          SizedBox(
-            width: MediaQuery.sizeOf(context).width -
-                (KHorizentalPadding * 2) -
-                48,
-            child: Text.rich(
-              TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'من خلال إنشاء حساب ، فإنك توافق على ',
-                    style: TextStyles.semiBold13.copyWith(
-                      color: const Color(0xFF949D9E),
-                    ),
+    return Row(
+      children: [
+        CustomCheckBox(
+          isChecked: isTermesAccepted,
+          onChecked: (value) {
+            isTermesAccepted = value;
+            setState(() {});
+          },
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        Expanded(
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: 'من خلال إنشاء حساب ، فإنك توافق على ',
+                  style: TextStyles.semiBold13.copyWith(
+                    color: const Color(0xFF949D9E),
                   ),
-                  TextSpan(
-                    text: 'الشروط والأحكام',
-                    style: TextStyles.semiBold13.copyWith(
-                      color: AppColors.lightprimaryColor,
-                    ),
+                ),
+                TextSpan(
+                  text: 'الشروط والأحكام',
+                  style: TextStyles.semiBold13.copyWith(
+                    color: AppColors.lightprimaryColor,
                   ),
-                  const TextSpan(
-                    text: ' ',
-                    style: TextStyles.semiBold13,
+                ),
+                const TextSpan(
+                  text: ' ',
+                  style: TextStyles.semiBold13,
+                ),
+                TextSpan(
+                  text: 'الخاصة',
+                  style: TextStyles.semiBold13.copyWith(
+                    color: AppColors.lightprimaryColor,
                   ),
-                  TextSpan(
-                    text: 'الخاصة',
-                    style: TextStyles.semiBold13.copyWith(
-                      color: AppColors.lightprimaryColor,
-                    ),
-                  ),
-                  const TextSpan(
-                    text: ' ',
-                    style: TextStyles.semiBold13,
-                  ),
-                  TextSpan(
-                    text: 'بنا',
-                    style: TextStyles.semiBold13
-                        .copyWith(color: AppColors.lightprimaryColor),
-                  ),
-                ],
-              ),
-              textAlign: TextAlign.right,
+                ),
+                const TextSpan(
+                  text: ' ',
+                  style: TextStyles.semiBold13,
+                ),
+                TextSpan(
+                  text: 'بنا',
+                  style: TextStyles.semiBold13
+                      .copyWith(color: AppColors.lightprimaryColor),
+                ),
+              ],
             ),
+            textAlign: TextAlign.right,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
