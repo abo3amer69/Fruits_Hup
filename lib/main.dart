084cb2f21822319034b2f18850d89569c5893270
Ahmed <01099517574med@gmail.com>
2025-02-02 01:50:59 +0200

    add project to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hup/core/helper_function/on_generate_routes.dart';
 import 'package:fruits_hup/core/services/shared_preferences_singelton.dart';
 import 'package:fruits_hup/core/utils/app_colors.dart';
 import 'package:fruits_hup/features/splash/presentation/views/splash_view.dart';
+import 'package:fruits_hup/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'generated/l10n.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized();
   await prefs.init();
   runApp(const FruitHup());
