008c0a35fcad908b3c129d158c4fa9344b246a36
Ahmed <01099517574med@gmail.com>
2025-02-03 16:37:34 +0200

    add get it

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hup/core/services/get_it_services.dart';
 import 'package:fruits_hup/core/widget/custom_app_bar.dart';
+import 'package:fruits_hup/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruits_hup/features/auth/presentation/cubit/signup_cubits/signup_cubit.dart';
 import 'package:fruits_hup/features/auth/presentation/views/widget/signup_view_body.dart';
 
 class SignupView extends StatelessWidget {
@@ -8,9 +12,14 @@ class SignupView extends StatelessWidget {
   static const routeName = 'signup';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: BuildAppBar(context, title: 'تسجيل حساب جديد'),
-      body: SignupViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(
+        getIt<AuthRepo>(),
+      ),
+      child: Scaffold(
+        appBar: BuildAppBar(context, title: 'تسجيل حساب جديد'),
+        body: const SignupViewBody(),
+      ),
     );
   }
 }
