3e2a2f2d6a6a3156a78f76f3bb3dd373861cd10e
Ahmed Amer <01099517574med@gmail.com>
2025-02-16 21:34:55 +0200

    create shared preferances singelton

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:fruite_hup/core/services/shared_preferences_singleton.dart';
 import 'package:fruite_hup/features/splash/presentation/views/splash_view.dart';
 import 'core/helper_function/on_generate_route.dart';
 import 'generated/l10n.dart';
 
-void main() {
+void main() async {
+  await Prefs.init();
   runApp(const FruitHup());
 }
 
