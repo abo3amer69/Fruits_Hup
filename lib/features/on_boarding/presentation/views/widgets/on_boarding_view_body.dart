864a93944ce6c31b47e66b871ff0739a0554afd1
Ahmed Amer <01099517574med@gmail.com>
2025-02-16 18:48:23 +0200

    create custom button and add app colors and ad text styles

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,4 +1,8 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruite_hup/constants.dart';
+import 'package:fruite_hup/core/utils/app_colors.dart';
+import 'package:fruite_hup/core/widgets/custom_button.dart';
 import 'package:fruite_hup/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -8,7 +12,31 @@ class OnBoardingViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(child: OnBoardingPageView()),
+        Expanded(
+          child: OnBoardingPageView(),
+        ),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColors.primaryColor,
+            color: AppColors.primaryColor.withOpacity(0.5),
+          ),
+        ),
+        SizedBox(
+          height: 29,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: kHorizintalPadding,
+          ),
+          child: CustomButton(
+            onPressed: () {},
+            text: 'ابدأ الان',
+          ),
+        ),
+        SizedBox(
+          height: 43,
+        ),
       ],
     );
   }
