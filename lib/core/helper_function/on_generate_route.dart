15a5f90ca688960dffbb13e60324d93b4e515582
Ahmed Amer <01099517574med@gmail.com>
2025-02-16 16:43:51 +0200

    splash view logic ui done

--- lib/core/helper_function/on_generate_route.dart
+++ lib/core/helper_function/on_generate_route.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fruite_hup/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:fruite_hup/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
 
     default:
       return MaterialPageRoute(builder: (context) => Scaffold());
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fruite_hup/core/utils/app_images.dart';
+import 'package:fruite_hup/features/on_boarding/presentation/views/on_boarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    exCuteNavigation();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -24,5 +35,12 @@ class SplashViewBody extends StatelessWidget {
         ),
       ],
     );
+  }
+  
+  void exCuteNavigation() {
+    Future.delayed( const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+      
+    });
   }
 }
