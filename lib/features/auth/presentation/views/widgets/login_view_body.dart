5c9fb3dfced493b699e5a132d0acd06d23f1f6a4
Ahmed Amer <01099517574med@gmail.com>
2025-02-17 02:18:32 +0200

    login ui done "social button"

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,72 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:fruite_hup/constants.dart';
 import 'package:fruite_hup/core/utils/app_colors.dart';
+import 'package:fruite_hup/core/utils/app_images.dart';
 import 'package:fruite_hup/core/utils/app_text_styles.dart';
 import 'package:fruite_hup/core/widgets/custom_button.dart';
 import 'package:fruite_hup/core/widgets/custom_text_field.dart';
 import 'package:fruite_hup/features/auth/presentation/views/widgets/dont_have_an_account_widget.dart';
 import 'package:fruite_hup/features/auth/presentation/views/widgets/or_divider.dart';
+import 'package:fruite_hup/features/auth/presentation/views/widgets/social_login_button.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: kHorizintalPadding,
-      ),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 24,
-            ),
-            CustomTextFormField(
-              hintText: 'البريد الإلكتروني',
-              textInputType: TextInputType.emailAddress,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            CustomTextFormField(
-              suffixIcon: Icon(
-                Icons.remove_red_eye,
-                color: Color(0xffC9CECF),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: kHorizintalPadding,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 24,
               ),
-              hintText: 'كلمة المرور',
-              textInputType: TextInputType.visiblePassword,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'نسيت كلمة المرور؟',
-                  style: TextStyles.semiBold13.copyWith(
-                    color: AppColors.lightPrimaryColor,
-                  ),
+              CustomTextFormField(
+                hintText: 'البريد الإلكتروني',
+                textInputType: TextInputType.emailAddress,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                suffixIcon: Icon(
+                  Icons.remove_red_eye,
+                  color: Color(0xffC9CECF),
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 33,
-            ),
-            CustomButton(
-              onPressed: () {},
-              text: 'تسجيل الدخول',
-            ),
-            SizedBox(
-              height: 33,
-            ),
-            DontHaveAnAccountWidget(),
-            SizedBox(
-              height: 33,
-            ),
-            OrDivider(),
-          ],
+                hintText: 'كلمة المرور',
+                textInputType: TextInputType.visiblePassword,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'نسيت كلمة المرور؟',
+                    style: TextStyles.semiBold13.copyWith(
+                      color: AppColors.lightPrimaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 33,
+              ),
+              CustomButton(
+                onPressed: () {},
+                text: 'تسجيل الدخول',
+              ),
+              SizedBox(
+                height: 33,
+              ),
+              DontHaveAnAccountWidget(),
+              SizedBox(
+                height: 33,
+              ),
+              
+              OrDivider(),
+              SizedBox(
+                height: 16,
+              ),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.assetsImagesGoogleIcon,
+                title: 'تسجيل الدخول باستخدام جوجل',
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.assetsImagesAppIcon,
+                title: 'تسجيل الدخول باستخدام ابل',
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.assetsImagesFacebookIcon,
+                title: 'تسجيل الدخول باستخدام فيسبوك',
+              ),
+            ],
+          ),
         ),
       ),
     );
